260ea059dc653c90a8dc57a79610de8d79ae1e46
Kasper Heuer <40208910@roctilburg.nl>
2025-02-25 14:29:21 +0100

    csrf() ruzie

--- example/resources/views/jobs/index.blade.php
+++ example/resources/views/jobs/index.blade.php
@@ -5,7 +5,7 @@
     <h1>Job Listings</h1>
     <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{ $job['Id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
                 <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
                 <div class="">
                     <strong>{{ $job['title'] }}</strong>: pays € {{ $job['salery'] }} per year.
--- example/routes/web.php
+++ example/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use App\Http\Controllers\customer\AuthController;
 
 Route::get('/', function () {
     return view(
@@ -9,22 +10,29 @@ Route::get('/', function () {
     );
 });
 
+
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->simplePaginate(3);
-    return view("jobs", [
+    return view("jobs.index", [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view("jobs.create");
+});
 
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view("job", ["job" => $job]);
+    return view("jobs.show", ["job" => $job]);
 });
 
 
+Route::post('/jobs', function () {
+    dd(request()->all());
+});
 
 Route::get('/contact', function () {
     return view("contact");
